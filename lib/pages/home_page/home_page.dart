4959d54934f8c43531cf7666e358e94b15310c02
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-10-04 22:34:02 -0300

    Criado estrutura inicial do projeto

---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:ajustamento_curva_minimo/widgets/table_calculator/table_calculator.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  static final a11 = ValueNotifier<int>(0);
+  static final a12 = ValueNotifier<int>(0);
+  static final a13 = ValueNotifier<int>(0);
+  static final a21 = ValueNotifier<int>(0);
+  static final a22 = ValueNotifier<int>(0);
+  static final a23 = ValueNotifier<int>(0);
+  static final a31 = ValueNotifier<int>(0);
+  static final a32 = ValueNotifier<int>(0);
+  static final a33 = ValueNotifier<int>(0);
+  static final sum = ValueNotifier<int?>(null);
+  static final is3x3 = ValueNotifier<bool>(false);
+
+  String get _qty => is3x3.value ? '3x3' : '2x2';
+  static String get invertedQty => is3x3.value ? '2x2' : '3x3';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: ValueListenableBuilder(
+          valueListenable: is3x3,
+          builder: (_, __, ___) => Text('Calcular determinante $_qty'),
+        ),
+        actions: [
+          TextButton(
+            child: ValueListenableBuilder(
+                valueListenable: is3x3,
+                builder: (_, __, ___) => Text(
+                invertedQty,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            onPressed: () {
+              is3x3.value = !is3x3.value;
+              sum.value = null;
+            },
+          ),
+        ],
+      ),
+      body: TableCalculator(),
+    );
+  }
+}
---
+++ lib/widgets/table_calculator/table_calculator.dart
@@ -0,0 +1,196 @@
+import 'package:ajustamento_curva_minimo/pages/home_page/home_page.dart';
+import 'package:ajustamento_curva_minimo/widgets/column_cell/column_cell.dart';
+import 'package:flutter/material.dart';
+
+class TableCalculator extends StatelessWidget {
+  TableCalculator({
+    Key? key,
+  }) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+                valueListenable: HomePage.is3x3,
+                builder: (_, __, ___) => Column(
+                  children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Form(
+                        key: _formKey,
+                        child: DataTable(
+                          columnSpacing: 10,
+                          columns: <DataColumn>[
+                            const DataColumn(
+                              label: Text(
+                                'Coluna 1',
+                                style: TextStyle(fontStyle: FontStyle.italic),
+                              ),
+                            ),
+                            const DataColumn(
+                              label: Text(
+                                'Coluna 2',
+                                style: TextStyle(fontStyle: FontStyle.italic),
+                              ),
+                            ),
+                            if (HomePage.is3x3.value)
+                              const DataColumn(
+                                label: Text(
+                                  'Coluna 3',
+                                  style: TextStyle(fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                          ],
+                          rows: <DataRow>[
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(
+                                  ColumnCell(
+                                    onChanged: (value) {
+                                      if (int.tryParse(value) != null) {
+                                        HomePage.a11.value = int.parse(value);
+                                      }
+                                    },
+                                  ),
+                                ),
+                                DataCell(
+                                  ColumnCell(
+                                    onChanged: (value) {
+                                      if (int.tryParse(value) != null) {
+                                        HomePage.a12.value = int.parse(value);
+                                      }
+                                    },
+                                  ),
+                                ),
+                                if (HomePage.is3x3.value)
+                                  DataCell(
+                                  ColumnCell(
+                                    onChanged: (value) {
+                                      if (int.tryParse(value) != null) {
+                                        HomePage.a13.value = int.parse(value);
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(
+                                  ColumnCell(
+                                    onChanged: (value) {
+                                      if (int.tryParse(value) != null) {
+                                        HomePage.a21.value = int.parse(value);
+                                      }
+                                    },
+                                  ),
+                                ),
+                                DataCell(
+                                  ColumnCell(
+                                    onChanged: (value) {
+                                      if (int.tryParse(value) != null) {
+                                        HomePage.a22.value = int.parse(value);
+                                      }
+                                    },
+                                  ),
+                                ),
+                                if (HomePage.is3x3.value)
+                                  DataCell(
+                                    ColumnCell(
+                                      onChanged: (value) {
+                                        if (int.tryParse(value) != null) {
+                                          HomePage.a23.value = int.parse(value);
+                                        }
+                                      },
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            if (HomePage.is3x3.value)
+                              DataRow(
+                                cells: <DataCell>[
+                                  DataCell(
+                                    ColumnCell(
+                                      onChanged: (value) {
+                                        if (int.tryParse(value) != null) {
+                                          HomePage.a31.value = int.parse(value);
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  DataCell(
+                                    ColumnCell(
+                                      onChanged: (value) {
+                                        if (int.tryParse(value) != null) {
+                                          HomePage.a32.value = int.parse(value);
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  if (HomePage.is3x3.value)
+                                    DataCell(
+                                    ColumnCell(
+                                      onChanged: (value) {
+                                        if (int.tryParse(value) != null) {
+                                          HomePage.a33.value = int.parse(value);
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    ElevatedButton(
+                      onPressed: () {
+                        HomePage.sum.value = null;
+                        if (_formKey.currentState!.validate()) {
+                          if (HomePage.is3x3.value) {
+                            final value = (HomePage.a11.value * HomePage.a22.value * HomePage.a33.value) + 
+                              (HomePage.a12.value * HomePage.a23.value * HomePage.a31.value) + 
+                                (HomePage.a13.value * HomePage.a21.value * HomePage.a32.value) - 
+                                  (HomePage.a13.value * HomePage.a22.value * HomePage.a31.value) - 
+                                    (HomePage.a11.value * HomePage.a23.value * HomePage.a32.value) - 
+                                      (HomePage.a12.value * HomePage.a21.value * HomePage.a33.value);
+
+                            HomePage.sum.value = value;
+                          }
+                          else {
+                            final value = (HomePage.a11.value * HomePage.a22.value) - 
+                              (HomePage.a12.value * HomePage.a21.value);
+
+                            HomePage.sum.value = value;
+                          }
+                        }
+                      }, 
+                      child: const Text('Calcular')
+                    ),
+                    const SizedBox(height: 10,),
+                    ValueListenableBuilder(
+                      valueListenable: HomePage.sum, 
+                      builder: (_, __, ___) => Text(
+                        HomePage.sum.value != null ? 'Determinante = ${HomePage.sum.value}' : '',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
